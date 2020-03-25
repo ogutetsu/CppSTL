b12fb5560609e9f431db130286b35e65fb4c85af
og <oguogu@test.com>
2020-03-25 14:09:30 +0900

    [update] Lambdaのサンプル追加

--- FunctionObject.cpp
+++ FunctionObject.cpp
@@ -21,6 +21,10 @@ void FunctionObjectMain()
 	for (auto v : vec) cout << v << " ";
 	cout << endl;
 	
-
+	std::vector<int> vec1{ 1,2,3,4,5,6,7,8,9,10 };
+	std::for_each(vec1.begin(), vec1.end(), [](int& i) { i = i * i; });
+	
+	for (auto v : vec1) cout << v << " ";
+	cout << endl;
 }
 
