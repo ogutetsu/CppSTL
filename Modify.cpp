306dd42f3ec5d5e0c3b4b2062f84c7fb98a34c3b
og <oguogu@test.com>
2020-03-31 15:45:26 +0900

    [update] ModifyにReplaceSampleを追加

--- Modify.cpp
+++ Modify.cpp
@@ -27,8 +27,31 @@ void CopySample()
 	cout << "str : " << str << endl;
 }
 
+void ReplaceMain()
+{
+	std::string str{ "Only for testing purpose" };
+	std::replace(str.begin(), str.end(), ' ', '1');
+	cout << "replace \' \' to \'1\' : " << str << endl;
+
+	std::replace_if(str.begin(), str.end(), [](char c) {return c == '1'; }, '2');
+	cout << "replace_if \'1\' to \'2\' : " << str << endl;
+
+	std::string str2;
+	std::replace_copy(str.begin(), str.end(), std::back_inserter(str2), '2', '3');
+	cout << "replace_copy \'2\' to \'3\' : " << str2 << endl;
+	
+	std::string str3;
+	std::replace_copy_if(str2.begin(), str2.end(),
+	                     std::back_inserter(str3), [](char c) { return c == '3'; }, '4');
+	cout << "replace_copy_if \'3\' to \'4\' : " << str3 << endl;
+}
+
 void ModifyMain()
 {
 	CopySample();
+
+	ReplaceMain();
+
+	
 }
 
