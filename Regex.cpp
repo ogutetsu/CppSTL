8e4ad3f51359954a43bd9b5c3bd7b96ea8b88191
og <oguogu@test.com>
2020-04-15 12:00:59 +0900

    [update] RegexにMatchSampleを追加

--- Regex.cpp
+++ Regex.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <regex>
 
@@ -29,9 +30,59 @@ void RegexObjectSample()
 	}
 }
 
+void RegexMatchSample()
+{
+	std::function<void(const std::string, const std::string)> showCaptureGroups =
+		[](const std::string regEx, std::string text)
+	{
+		std::regex rgx(regEx);
+		std::smatch smatch;
+		if(std::regex_search(text, smatch, rgx))
+		{
+			cout << "regEx: " << regEx << " text: " << text << " " << smatch[0] << " " << smatch[1]
+				<< " " << smatch[2] << " " << smatch[3] << endl;
+		}
+		
+	};
+
+
+	showCaptureGroups("abc+", "abccccc");
+	showCaptureGroups("(a+)(b+)", "aaabccc");
+	showCaptureGroups("((a+)(b+))", "aaabccc");
+	showCaptureGroups("(ab)(abc)+", "ababcabc");
+	
+
+	std::string privateAddress = "192.168.178.21";
+	std::string regEx(R"((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))");
+	std::regex rgx(regEx);
+	std::smatch smatch;
+
+	if(std::regex_match(privateAddress, smatch, rgx))
+	{
+		for(auto cap : smatch)
+		{
+			cout << "capture group: " << cap << endl;
+			if(cap.matched)
+			{
+				std::for_each(cap.first, cap.second, [](int v)
+				{
+					cout << std::hex << v << " ";
+				});
+				cout << endl;
+			}
+		}
+	}
+}
+
 void RegexMain()
 {
 	RegexObjectSample();
+
+	RegexMatchSample();
+	
+
+
+	
 }
 
 
