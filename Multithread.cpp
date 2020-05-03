277072ff03fe838ecab7e357b529784a63048ce4
og <oguogu@test.com>
2020-05-03 16:56:56 +0900

    [update] MultithreadにThreadSampleを追加

--- Multithread.cpp
+++ Multithread.cpp
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <functional>
 #include <thread>
+#include <string>
 
 
 using std::cout;
@@ -38,10 +39,80 @@ void AtomicSample()
 
 }
 
+
+class HelloFuncObject
+{
+public:
+	void operator()() const
+	{
+		cout << "function object" << endl;
+	}
+};
+
+
+void ThreadSample()
+{
+
+	std::function<void(void)> helloFunc = []()
+	{
+		cout << "function" << endl;
+	};
+
+	std::thread t1(helloFunc);
+
+	HelloFuncObject hello_func_object;
+	std::thread t2(hello_func_object);
+
+	std::thread t3([] {cout << "lambda function" << endl;  });
+
+
+	t1.join();
+	t2.join();
+	t3.join();
+
+
+	std::function<void(std::string)> printStringCopy = [](std::string s)
+	{
+		cout << s << endl;
+	};
+
+	std::string s{ "C++" };
+	std::thread strCopy(printStringCopy, s);
+	std::thread strCopy2([=] {cout << s << endl; });
+	strCopy.join();
+	strCopy2.join();
+
+
+	cout << "thread::hardware_concurrency: " << std::thread::hardware_concurrency() << endl;
+
+	std::thread t4([] { cout << "t4 : " << std::this_thread::get_id() << endl; });
+	std::thread t5([] { cout << "t5 : " << std::this_thread::get_id() << endl; });
+	cout << "t4.get_id : " << t4.get_id() << endl;
+	cout << "t5.get_id : " << t5.get_id() << endl;
+
+	t4.swap(t5);
+	cout << "  swap  " << endl;
+	cout << "t4.get_id : " << t4.get_id() << endl;
+	cout << "t5.get_id : " << t5.get_id() << endl;
+
+
+	cout << "this_thread::get_id : " << std::this_thread::get_id() << endl;
+
+
+	t4.join();
+	t5.join();
+
+}
+
+
+
 void MultithreadMain()
 {
 
 	AtomicSample();
+
+	ThreadSample();
+
 	
 	
 }
